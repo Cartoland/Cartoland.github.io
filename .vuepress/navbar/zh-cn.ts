d82092c2f1db6522a83687e3562b03072c500e12
LinLei_Baruch <48636379+LinLei0102@users.noreply.github.com>
2024-06-27 08:51:18 +0800

    Create zh-cn.ts

---
+++ .vuepress/navbar/zh-cn.ts
@@ -0,0 +1,9 @@
+import { navbar } from "vuepress-theme-hope";
+
+export default navbar([
+  "/zh-tw/",
+  { text: "地图", icon: "list-ol", link: "/zh-tw/posts/maps/" },
+  { text: "资源包", icon: "list-ol", link: "/zh-tw/posts/resource-packs/" },
+  { text: "资料包", icon: "list-ol", link: "/zh-tw/posts/data-packs/" },
+  { text: "活动", icon: "list-ol", link: "/zh-tw/posts/activities/" },
+]);
