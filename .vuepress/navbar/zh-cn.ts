61e22b3e76ac0dc87aaf7555c0d8eb43c18413f1
LinLei_Baruch <48636379+LinLei0102@users.noreply.github.com>
2024-06-28 18:55:20 +0800

    fix zh-cn.ts

--- .vuepress/navbar/zh-cn.ts
+++ .vuepress/navbar/zh-cn.ts
@@ -3,7 +3,7 @@ import { navbar } from "vuepress-theme-hope";
 export default navbar([
   "/zh-cn/",
   { text: "地图", icon: "list-ol", link: "/zh-cn/posts/maps/" },
-  { text: "资源包", icon: "list-ol", link: "/zh-cn/posts/resource-packs/" },
+  { text: "材质包", icon: "list-ol", link: "/zh-cn/posts/resource-packs/" },
   { text: "数据包", icon: "list-ol", link: "/zh-cn/posts/data-packs/" },
   { text: "活动", icon: "list-ol", link: "/zh-cn/posts/activities/" },
 ]);
